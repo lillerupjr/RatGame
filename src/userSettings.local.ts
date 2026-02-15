3a6fb5f76cf8b39d6b73b02de3b88e6aaa86b774
s214922 <lillerupjr@gmail.com>
2026-02-15 14:03:37 +0100

    user setting file

---
+++ src/userSettings.local.ts
@@ -0,0 +1,10 @@
+import type { UserSettingsPatch } from "./userSettings";
+
+const localSettings: UserSettingsPatch = {
+  debug: {
+    grid: true,
+    slices: true,
+  },
+};
+
+export default localSettings;
